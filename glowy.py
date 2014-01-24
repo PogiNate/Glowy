2271fcca9359e4966bbd15629f0fa4ee865bd23a
Nate Dickson <natedickson@ldschurch.org>
2014-01-24 09:42:12 -0700

    Create the project file and the memglow file.

---
+++ glowy.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env Python
+"""
+For now this just calls the three modules. Once I learn how to process command line arguments I'll augment this to handle those as well.
+"""
+__author__ = 'Nate Dickson'
+#import sys
+#import getopt
+import cpuglow
+
+
+def main():
+    cpuglow.equalizer()
+
+
+if __name__ == '__main__':
+    main()
