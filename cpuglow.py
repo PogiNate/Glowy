99ef2d30a063a83d6e688a445eecbeb7bc09c0fe
Nate Dickson <natedickson@ldschurch.org>
2014-01-24 10:10:52 -0700

    Update cpuglow and memglow

--- cpuglow.py
+++ cpuglow.py
@@ -1,78 +1,79 @@
 #!/usr/bin/env python
-'''
+"""
 
 this module makes it possible to make a specific pattern of LEDS
-light up based on CPU usage. This module will only work correctly on a 
+light up based on CPU usage. This module will only work correctly on a
 Raspberry Pi with the PiGlow accessory board correctly installed and configured.
 
-'''
+"""
 
 import os, sys
 from time import sleep
 from pyglow import PyGlow
 import psutil
+
 pg = PyGlow()
 
-trail = [128,64,32]
+trail = [128, 64, 32]
+
 
 def snake():
-  '''
+    """
 
-  Makes the lights along the CPU arm light up sequentally. The amount of time between 
-  runs is reduced as the cpu utilization increases.
+      Makes the lights along the CPU arm light up sequentially. The amount of time between
+      runs is reduced as the cpu utilization increases.
 
-  '''
-  
-  leds = [1,2,3,4,5,6]
-  try:
-    while True:
-      cpu = psutil.cpu_percent()
-      delay = 1 - cpu * 0.01 
-      for i in range(len(leds)+3): 
-        head = i
-        mid  = i - 1
-        tail = i - 2
+     """
 
-        for i in range(len(leds)):
-          if i == head:
-            pg.led(leds[i], trail[0])
-          elif i == mid:
-            pg.led(leds[i], trail[1])
-          elif i == tail:
-            pg.led(leds[i], trail[2])
-          else:
-            pg.led(leds[i],0)
-          sleep(0.01)
-      sleep(delay)
-  except KeyboardInterrupt:
-    pg.all(0)
+    leds = [1, 2, 3, 4, 5, 6]
+    try:
+        while True:
+            cpu = psutil.cpu_percent()
+            delay = 1 - cpu * 0.01
+            for i in range(len(leds) + 3):
+                head = i
+                mid = i - 1
+                tail = i - 2
+
+                for i in range(len(leds)):
+                    if i == head:
+                        pg.led(leds[i], trail[0])
+                    elif i == mid:
+                        pg.led(leds[i], trail[1])
+                    elif i == tail:
+                        pg.led(leds[i], trail[2])
+                    else:
+                        pg.led(leds[i], 0)
+                    sleep(0.01)
+            sleep(delay)
+    except KeyboardInterrupt:
+        pg.all(0)
+
 
 def equalizer():
-  '''
+    """
 
-  Lights from the center outward, illuminating more lights as the CPU usage increases.
+      Lights from the center outward, illuminating more lights as the CPU usage increases.
 
-  '''
-  leds = [6,5,4,3,2,1]
-  try:
-    while True:
-      level = 100 / len(leds)
-      cpu = psutil.cpu_percent()
-      brightness = 70
+    """
+    leds = [6, 5, 4, 3, 2, 1]
+    try:
+        while True:
+            level = 100 / len(leds)
+            cpu = psutil.cpu_percent()
+            brightness = 70
 
-      for i in range(len(leds)):
-        if (cpu >= level * i):
-          pg.led(leds[i],brightness)
-        else:
-          pg.led(leds[i],0)
-      sleep(0.2)
-      
-  except KeyboardInterrupt:
-    pg.all(0)
+            for i in range(len(leds)):
+                if (cpu >= level * i):
+                    pg.led(leds[i], brightness)
+                else:
+                    pg.led(leds[i], 0)
+            sleep(0.2)
+
+    except KeyboardInterrupt:
+        pg.all(0)
 
 
 if __name__ == "__main__":
-  import sys
-  #TODO: Replace the following with the "EQ" cpu mode.
-  #equalizer()
-  snake()
+    #equalizer()
+    snake()
--- memglow.py
+++ memglow.py
@@ -1,1 +1,42 @@
-__author__ = 'natedickson'
+#!/usr/bin/env python
+"""
+
+We'll light up the second arm of the PiGlow based on memory usage.
+Naturally this requires the PiGlow accessory to be properly installed and configured.
+
+"""
+__author__ = 'Nate Dickson'
+
+from time import sleep
+from pyglow import PyGlow
+import psutil
+
+pg = PyGlow()
+
+
+def pulse():
+
+    """
+
+    This will pulse the lights up to the current memory usage for every
+    check of the memory usage level.
+
+    """
+
+    arm = [7, 8, 9, 10, 11, 12]
+
+    try:
+        while True:
+            used = psutil.phymem_usage()[3]
+            brightness = 70
+            speed = 300
+            level = 100/len(arm)
+            for i in range(len(arm)):
+                if used >= level * i:
+                    pg.pulse(arm[i], brightness, speed)
+            sleep(0.2)
+    except KeyboardInterrupt:
+        pg.all(0)
+
+if __name__ == "__main__":
+    pulse()
